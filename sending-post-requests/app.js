3aaa1f622c48b28ea01836ce0795ec50fd44b4a6
Rohit Kumawat <rkumawat125@gmail.com>
2019-04-08 13:14:42 +0530

    Post model, error handling, serving image folder statically

--- sending-post-requests/app.js
+++ sending-post-requests/app.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -7,6 +10,8 @@ const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,4 +22,17 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({message: message});
+});
+
+mongoose.connect('mongodb+srv://rohit_kumawat:cunltC77NOGz1jqS@ecommerce-rs4wl.mongodb.net/messages')
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- sending-post-requests/controllers/feed.js
+++ sending-post-requests/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -17,29 +19,35 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
-  const errors = validatonResult(req);
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-            .status(422)
-            .json({
-              message: 'Validation failed, entered data is incorrect',
-              errors: errors.array()
-            })
+    const error = new Error("validation failed, entered data is incorrect");
+    error.statusCode = 422;
+    throw error;
   }
+
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: 
-      {
-        _id: new Date().toISOString(), 
-        title: title, 
-        content: content,
-        creator: {
-          name: 'rk'
-        },
-        createdAt: new Date()
-      }
-  });
+  const post = new Post({
+    title: title, 
+    content: content,
+    imageUrl: 'images/animate.jpg',
+    creator: {
+      name: 'rk'
+    }
+  })
+  post.save()
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+          message: 'Post created successfully!',
+          post: result
+        });
+      })
+      .catch(err => {
+        if (!error.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err)
+      })
 };
